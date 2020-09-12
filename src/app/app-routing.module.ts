e36a32bf174b69ea69c669906e1f522dd08285cf
matsu <50922910+mtsml@users.noreply.github.com>
2020-09-12 22:28:52 +0900

    動画の詳細画面を追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { ChannelComponent } from './channel/channel.component';
+import { VideoComponent } from './video/video.component';
 
 const routes: Routes = [
     { path: '', component: IndexComponent },
-    { path: 'channel/:channel_id', component: ChannelComponent }
+    { path: 'channel/:channel_id', component: ChannelComponent },
+    { path: 'video/:video_id', component: VideoComponent }
 ]
 
 @NgModule({
